b7778893589c0da0edc0023ec4ea36d27a65ad96
Alexander O'Mara <me@alexomara.com>
2025-04-14 00:04:41 -0400

    Format type and comments

--- format.ts
+++ format.ts
@@ -4,7 +4,25 @@
  * Plist formats.
  */
 
-// CFPropertyListFormat:
+/**
+ * Legacy OpenStep Plist format.
+ */
 export const FORMAT_OPEN_STEP = 1;
+
+/**
+ * XML Plist format, version 1.0.
+ */
 export const FORMAT_XML_V1_0 = 100;
+
+/**
+ * Binary Plist format, version 1.0.
+ */
 export const FORMAT_BINARY_V1_0 = 200;
+
+/**
+ * Plist formats.
+ */
+export type Format =
+	| typeof FORMAT_OPEN_STEP
+	| typeof FORMAT_XML_V1_0
+	| typeof FORMAT_BINARY_V1_0;
