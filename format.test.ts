8b9cfea5a0953605763365d4b4fa0dd19b6fb146
Alexander O'Mara <me@alexomara.com>
2025-04-13 23:56:57 -0400

    Added kCFPropertyListOpenStepFormat to test

--- format.test.ts
+++ format.test.ts
@@ -7,6 +7,7 @@ Deno.test('format constants', () => {
 	const keys = Object.keys(FORMAT).filter((s) => regCFPLF.test(s)).sort();
 	assertEquals(keys, [
 		'kCFPropertyListBinaryFormat_v1_0',
+		'kCFPropertyListOpenStepFormat',
 		'kCFPropertyListXMLFormat_v1_0',
 	]);
 
--- format.ts
+++ format.ts
@@ -4,10 +4,9 @@
  * Plist formats.
  */
 
+// CFPropertyListFormat:
 export const kCFPropertyListOpenStepFormat = 1;
-
 // deno-lint-ignore camelcase
 export const kCFPropertyListXMLFormat_v1_0 = 100;
-
 // deno-lint-ignore camelcase
 export const kCFPropertyListBinaryFormat_v1_0 = 200;
