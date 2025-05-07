d275cf6e59972f08fae00e6fc3d981a439e2b146
Alexander O'Mara <me@alexomara.com>
2025-05-06 21:02:46 -0400

    Type type

---
+++ type.ts
@@ -0,0 +1,15 @@
+/**
+ * @module
+ *
+ * Variable type.
+ */
+
+/**
+ * Variable type.
+ */
+export interface PLType {
+	/**
+	 * Variable type name.
+	 */
+	readonly [Symbol.toStringTag]: string;
+}
