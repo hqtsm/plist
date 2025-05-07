84390dbb381f9663c4d9ebe4c840c978278293b2
Alexander O'Mara <me@alexomara.com>
2025-05-07 01:39:29 -0400

    Added PLData

---
+++ data.ts
@@ -0,0 +1,56 @@
+import type { PLType } from './type.ts';
+
+let buffers: WeakMap<PLData, ArrayBuffer>;
+
+const type = 'PLData';
+
+/**
+ * Property list data type.
+ */
+export class PLData implements PLType {
+	declare public readonly [Symbol.toStringTag]: string;
+
+	/**
+	 * Create property list data reference.
+	 *
+	 * @param byteLength Byte length.
+	 */
+	constructor(byteLength = 0) {
+		(buffers ??= new WeakMap()).set(this, new ArrayBuffer(byteLength));
+	}
+
+	/**
+	 * Get buffer.
+	 *
+	 * @returns Data buffer.
+	 */
+	public get buffer(): ArrayBuffer {
+		return buffers.get(this)!;
+	}
+
+	/**
+	 * Get byte length.
+	 *
+	 * @returns Byte length.
+	 */
+	public get byteLength(): number {
+		return buffers.get(this)!.byteLength;
+	}
+
+	/**
+	 * Check if type is data type.
+	 *
+	 * @param arg Property list type.
+	 * @returns True if type is data type.
+	 */
+	public static is(arg: PLType): arg is PLData {
+		return arg[Symbol.toStringTag] === type;
+	}
+
+	static {
+		Object.defineProperty(this.prototype, Symbol.toStringTag, {
+			value: type,
+			configurable: true,
+		});
+	}
+}
