60fc4e1a1971d24d93276d219a7440dd02a9c447
Alexander O'Mara <me@alexomara.com>
2025-05-24 02:26:58 -0400

    Cover PLReal

--- real.test.ts
+++ real.test.ts
@@ -7,19 +7,28 @@ const PI32 = Math.fround(PI64);
 
 Deno.test('initial value', () => {
 	assertEquals(new PLReal().value, 0);
+	assertEquals(new PLReal().bits, 64);
 	assertEquals(new PLReal(PI64).value, PI64);
+	assertEquals(new PLReal(0, 64).bits, 64);
 	assertEquals(new PLReal(PI64, 64).value, PI64);
+	assertEquals(new PLReal(0, 32).bits, 32);
 	assertEquals(new PLReal(PI64, 32).value, PI32);
+	assertEquals(new PLReal(PI64, 32).bits, 32);
 });
 
 Deno.test('set value', () => {
 	const real = new PLReal();
+	assertEquals(real.bits, 64);
 	real.value = PI64;
 	assertEquals(real.value, PI64);
+	real.bits = 32;
+	assertEquals(real.bits, 32);
 	real.bits = 32;
 	assertEquals(real.value, PI32);
 	real.value = PI64;
 	assertEquals(real.value, PI32);
+	real.bits = 64;
+	assertEquals(real.bits, 64);
 	real.bits = 64;
 	assertEquals(real.value, PI32);
 	real.value = PI64;
