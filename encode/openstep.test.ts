d00c19c834050791dbdb425eab5d9ea605e8ec32
Alexander O'Mara <me@alexomara.com>
2025-06-25 00:51:50 -0400

    Added the OpenStep encoder

---
+++ encode/openstep.test.ts
@@ -0,0 +1,482 @@
+import { assertEquals, assertThrows } from '@std/assert';
+import { fixturePlist } from '../spec/fixture.ts';
+import { PLArray } from '../array.ts';
+import { PLString } from '../string.ts';
+import { PLData } from '../data.ts';
+import { PLDict } from '../dict.ts';
+import { FORMAT_OPENSTEP, FORMAT_STRINGS } from '../format.ts';
+import type { PLType } from '../type.ts';
+import { encodeOpenStep } from './openstep.ts';
+
+function diff(a: Uint8Array, b: Uint8Array): number {
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] !== b[i]) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+Deno.test('Default format', () => {
+	assertEquals(
+		new Uint8Array(encodeOpenStep(new PLString())),
+		new Uint8Array(
+			encodeOpenStep(new PLString(), { format: FORMAT_OPENSTEP }),
+		),
+	);
+});
+
+Deno.test('Invalid format', () => {
+	assertThrows(
+		() => {
+			encodeOpenStep(new PLString(), {
+				format: 'UNKNOWN' as typeof FORMAT_OPENSTEP,
+			});
+		},
+		RangeError,
+		`Unknown format: UNKNOWN`,
+	);
+});
+
+Deno.test('Invalid strings root', () => {
+	assertThrows(
+		() => {
+			encodeOpenStep(new PLString(), { format: FORMAT_STRINGS });
+		},
+		TypeError,
+		`Invalid strings root type: ${new PLString()}`,
+	);
+});
+
+Deno.test('Circular reference: array', () => {
+	const array = new PLArray();
+	array.push(new PLDict([[new PLString('A'), array]]));
+	assertThrows(
+		() => {
+			encodeOpenStep(array);
+		},
+		TypeError,
+		'Circular reference',
+	);
+});
+
+Deno.test('Circular reference:  dict', () => {
+	const dict = new PLDict();
+	dict.set(new PLString('A'), new PLArray([dict]));
+	assertThrows(
+		() => {
+			encodeOpenStep(dict);
+		},
+		TypeError,
+		'Circular reference',
+	);
+});
+
+Deno.test('Custom indent', () => {
+	for (const format of [FORMAT_OPENSTEP, FORMAT_STRINGS] as const) {
+		for (const indent of ['  ', '\t\t', '\t ']) {
+			const dict = new PLDict([
+				[
+					new PLString('A'),
+					new PLDict([
+						[
+							new PLString('B'),
+							new PLDict([
+								[
+									new PLString('C'),
+									new PLDict([
+										[new PLString('D'), new PLString('E')],
+									]),
+								],
+							]),
+						],
+					]),
+				],
+			]);
+			const encode = encodeOpenStep(dict, { format, indent });
+			const indents = new TextDecoder().decode(encode).split('\n')
+				.map((s) => s.replace(/\S.*$/g, ''))
+				.filter((s) => s);
+			assertEquals(
+				indents,
+				format === FORMAT_STRINGS
+					? [
+						indent,
+						indent + indent,
+						indent + indent + indent,
+						indent + indent,
+						indent,
+					]
+					: [
+						indent,
+						indent + indent,
+						indent + indent + indent,
+						indent + indent + indent + indent,
+						indent + indent + indent,
+						indent + indent,
+						indent,
+					],
+				`${format}: ${JSON.stringify(indent)}`,
+			);
+		}
+	}
+});
+
+Deno.test('Invalid indent', () => {
+	assertThrows(
+		() => {
+			encodeOpenStep(new PLString(), { indent: '\n' });
+		},
+		RangeError,
+		'Invalid indent: "\\n"',
+	);
+});
+
+Deno.test('Invalid type', () => {
+	assertThrows(
+		() => {
+			encodeOpenStep({} as unknown as PLType);
+		},
+		TypeError,
+		`Invalid type: ${{}}`,
+	);
+});
+
+Deno.test('spec: array-0', async () => {
+	const encode = encodeOpenStep(new PLArray());
+	assertEquals(
+		encode,
+		await fixturePlist('array-0', 'openstep'),
+	);
+});
+
+Deno.test('spec: array-1', async () => {
+	const encode = encodeOpenStep(new PLArray([new PLString('A')]));
+	assertEquals(
+		encode,
+		await fixturePlist('array-1', 'openstep'),
+	);
+});
+
+Deno.test('spec: array-4', async () => {
+	const aa = new PLData(2);
+	new Uint8Array(aa.buffer).set(new Uint8Array([0x61, 0x61]));
+	const bb = new PLData(2);
+	new Uint8Array(bb.buffer).set(new Uint8Array([0x62, 0x62]));
+	const encode = encodeOpenStep(new PLArray([aa, bb, aa, bb]));
+	assertEquals(
+		encode,
+		await fixturePlist('array-4', 'openstep'),
+	);
+});
+
+Deno.test('spec: array-8', async () => {
+	const A = new PLString('A');
+	const B = new PLString('B');
+	const encode = encodeOpenStep(
+		new PLArray([A, B, A, B, A, B, A, B]),
+	);
+	assertEquals(
+		encode,
+		await fixturePlist('array-8', 'openstep'),
+	);
+});
+
+Deno.test('spec: array-26', async () => {
+	const encode = encodeOpenStep(
+		new PLArray(
+			[...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'].map((s) => new PLString(s)),
+		),
+	);
+	assertEquals(
+		encode,
+		await fixturePlist('array-26', 'openstep'),
+	);
+});
+
+Deno.test('spec: array-reuse', async () => {
+	const reuse = new PLArray([new PLString('AAAA'), new PLString('BBBB')]);
+	const encode = encodeOpenStep(new PLArray([reuse, reuse]));
+	assertEquals(
+		encode,
+		await fixturePlist('array-reuse', 'openstep'),
+	);
+});
+
+Deno.test('spec: data-0', async () => {
+	const encode = encodeOpenStep(new PLData());
+	assertEquals(
+		encode,
+		await fixturePlist('data-0', 'openstep'),
+	);
+});
+
+Deno.test('spec: data-1', async () => {
+	const data = new PLData(1);
+	new Uint8Array(data.buffer)[0] = 0x61;
+	const encode = encodeOpenStep(data);
+	assertEquals(
+		encode,
+		await fixturePlist('data-1', 'openstep'),
+	);
+});
+
+Deno.test('spec: data-2', async () => {
+	const data = new PLData(2);
+	new Uint8Array(data.buffer).set(new Uint8Array([0x61, 0x62]));
+	const encode = encodeOpenStep(data);
+	assertEquals(
+		encode,
+		await fixturePlist('data-2', 'openstep'),
+	);
+});
+Deno.test('spec: data-3', async () => {
+	const data = new PLData(3);
+	new Uint8Array(data.buffer).set(new Uint8Array([0x61, 0x62, 0x63]));
+	const encode = encodeOpenStep(data);
+	assertEquals(
+		encode,
+		await fixturePlist('data-3', 'openstep'),
+	);
+});
+
+Deno.test('spec: data-4', async () => {
+	const data = new PLData(4);
+	new Uint8Array(data.buffer).set(new Uint8Array([0x61, 0x62, 0x63, 0x64]));
+	const encode = encodeOpenStep(data);
+	assertEquals(
+		encode,
+		await fixturePlist('data-4', 'openstep'),
+	);
+});
+
+Deno.test('spec: data-256', async () => {
+	const bytes = new Uint8Array(256);
+	for (let i = 0; i < 256; i++) {
+		bytes[i] = i;
+	}
+	const data = new PLData(256);
+	new Uint8Array(data.buffer).set(bytes);
+	const encode = encodeOpenStep(data);
+	assertEquals(
+		encode,
+		await fixturePlist('data-256', 'openstep'),
+	);
+});
+
+Deno.test('spec: dict-empty', async () => {
+	const dict = new PLDict();
+	{
+		const encode = encodeOpenStep(dict);
+		assertEquals(
+			encode,
+			await fixturePlist('dict-empty', 'openstep'),
+		);
+	}
+	{
+		const encode = encodeOpenStep(dict, {
+			format: FORMAT_STRINGS,
+		});
+		assertEquals(
+			encode,
+			await fixturePlist('dict-empty', 'strings'),
+		);
+	}
+});
+
+Deno.test('spec: dict-chars', async () => {
+	const dict = new PLDict();
+	for (let i = 0; i <= 0xffff; i++) {
+		dict.set(
+			new PLString(String.fromCharCode(i)),
+			new PLString(String(i)),
+		);
+	}
+	{
+		const encode = encodeOpenStep(dict);
+		assertEquals(
+			diff(
+				encode,
+				await fixturePlist('dict-chars', 'openstep'),
+			),
+			-1,
+		);
+	}
+	{
+		const encode = encodeOpenStep(dict, {
+			format: FORMAT_STRINGS,
+		});
+		assertEquals(
+			diff(
+				encode,
+				await fixturePlist('dict-chars', 'strings'),
+			),
+			-1,
+		);
+	}
+});
+
+Deno.test('spec: dict-order', async () => {
+	const encode = encodeOpenStep(
+		new PLDict([
+			[new PLString(), new PLString('0')],
+			[new PLString('a'), new PLString('1')],
+			[new PLString('aa'), new PLString('2')],
+			[new PLString('aaa'), new PLString('3')],
+			[new PLString('ab'), new PLString('4')],
+			[new PLString('abb'), new PLString('5')],
+			[new PLString('ac'), new PLString('6')],
+		]),
+	);
+	assertEquals(
+		encode,
+		await fixturePlist('dict-order', 'openstep'),
+	);
+});
+
+Deno.test('spec: dict-reuse', async () => {
+	const reuse = new PLDict([
+		[new PLString('AAAA'), new PLString('1111')],
+		[new PLString('BBBB'), new PLString('2222')],
+	]);
+	const encode = encodeOpenStep(
+		new PLDict([
+			[new PLString('AA'), reuse],
+			[new PLString('BB'), reuse],
+		]),
+	);
+	assertEquals(
+		encode,
+		await fixturePlist('dict-reuse', 'openstep'),
+	);
+});
+
+Deno.test('spec: dict-repeat', async () => {
+	const encode = encodeOpenStep(
+		new PLDict([
+			[new PLString('A'), new PLString('11')],
+			[new PLString('B'), new PLString('21')],
+			[new PLString('B'), new PLString('22')],
+			[new PLString('C'), new PLString('32')],
+			[new PLString('C'), new PLString('31')],
+			[new PLString('C'), new PLString('33')],
+		]),
+	);
+	assertEquals(
+		encode,
+		await fixturePlist('dict-repeat', 'openstep'),
+	);
+});
+
+Deno.test('spec: string-empty', async () => {
+	const encode = encodeOpenStep(new PLString());
+	assertEquals(
+		encode,
+		await fixturePlist('string-empty', 'openstep'),
+	);
+});
+
+Deno.test('spec: string-ascii', async () => {
+	const encode = encodeOpenStep(new PLString('ASCII'));
+	assertEquals(
+		encode,
+		await fixturePlist('string-ascii', 'openstep'),
+	);
+});
+
+Deno.test('spec: string-chars', async () => {
+	const keys = [];
+	for (let i = 0; i <= 0xffff; i++) {
+		keys.push(String(i));
+	}
+	keys.sort();
+	const dict = new PLDict();
+	for (const key of keys) {
+		dict.set(new PLString(key), new PLString(String.fromCharCode(+key)));
+	}
+	{
+		const encode = encodeOpenStep(dict);
+		assertEquals(
+			diff(
+				encode,
+				await fixturePlist('string-chars', 'openstep'),
+			),
+			-1,
+		);
+	}
+	{
+		const encode = encodeOpenStep(dict, {
+			format: FORMAT_STRINGS,
+		});
+		assertEquals(
+			diff(
+				encode,
+				await fixturePlist('string-chars', 'strings'),
+			),
+			-1,
+		);
+	}
+});
+
+Deno.test('spec: string-unicode', async () => {
+	const encode = encodeOpenStep(new PLString('UTF\u20138'));
+	assertEquals(
+		encode,
+		await fixturePlist('string-unicode', 'openstep'),
+	);
+});
+
+Deno.test('spec: string-utf8-mb2-divide', async () => {
+	const encode = encodeOpenStep(new PLString('\u00f7'));
+	assertEquals(
+		encode,
+		await fixturePlist('string-utf8-mb2-divide', 'openstep'),
+	);
+});
+
+Deno.test('spec: string-utf8-mb2-ohm', async () => {
+	const encode = encodeOpenStep(new PLString('\u03a9'));
+	assertEquals(
+		encode,
+		await fixturePlist('string-utf8-mb2-ohm', 'openstep'),
+	);
+});
+
+Deno.test('spec: string-utf8-mb3-check', async () => {
+	const encode = encodeOpenStep(new PLString('\u2705'));
+	assertEquals(
+		encode,
+		await fixturePlist('string-utf8-mb3-check', 'openstep'),
+	);
+});
+
+Deno.test('spec: string-utf8-mb3-plus', async () => {
+	const encode = encodeOpenStep(new PLString('\uff0b'));
+	assertEquals(
+		encode,
+		await fixturePlist('string-utf8-mb3-plus', 'openstep'),
+	);
+});
+
+Deno.test('spec: string-utf8-mb4-robot', async () => {
+	const encode = encodeOpenStep(new PLString('\ud83e\udd16'));
+	assertEquals(
+		encode,
+		await fixturePlist('string-utf8-mb4-robot', 'openstep'),
+	);
+});
+
+Deno.test('spec: openstep-edge escapes-octal', async () => {
+	const encode = encodeOpenStep(
+		new PLDict([
+			[new PLString('null-0'), new PLString('\x000')],
+			[new PLString('null-8'), new PLString('\x008')],
+			[new PLString('oct16'), new PLString('\x0E')],
+			[new PLString('oct16-7'), new PLString('\x0E7')],
+		]),
+	);
+	assertEquals(
+		encode,
+		await fixturePlist('openstep-edge', 'escapes-octal'),
+	);
+});
---
+++ encode/openstep.ts
@@ -0,0 +1,379 @@
+import { PLArray } from '../array.ts';
+import { PLData } from '../data.ts';
+import { PLDict } from '../dict.ts';
+import { FORMAT_OPENSTEP, FORMAT_STRINGS } from '../format.ts';
+import { PLString } from '../string.ts';
+import type { PLType } from '../type.ts';
+
+const abtnvfr = 'abtnvfr';
+
+/**
+ * Check if a character is unquoted.
+ *
+ * @param chr Character code.
+ * @returns True if in: a-zA-Z0-9:_$/.-
+ */
+const unquoted = (chr: number) =>
+	chr < 123 && (
+		chr > 96 ||
+		(chr < 91 && (chr > 64 || (chr < 59 && (chr > 44 || chr === 36)))) ||
+		chr === 95
+	);
+
+/**
+ * Calculate string encode size.
+ *
+ * @param str String.
+ * @returns Size.
+ */
+function stringLength(str: string): number {
+	let i = str.length;
+	let total = i;
+	let quote = 0;
+	let next = 0;
+	let chr: number;
+	while (i--) {
+		if (!unquoted(chr = str.charCodeAt(i))) {
+			quote = 2;
+			total += chr < 32
+				? (
+					chr > 13
+						? (next < 56 && next > 47) as unknown as number + 2
+						: (chr < 7 && next < 56 && next > 47 ? 3 : 1)
+				)
+				: (
+					(chr === 34 || chr === 92) ||
+					(chr > 126 && (chr === 127 ? 3 : 5))
+				) as number;
+		}
+		next = chr;
+	}
+	return quote + total || 2;
+}
+
+/**
+ * Encode string into buffer.
+ *
+ * @param str String.
+ * @param dest Buffer.
+ * @param start Offset.
+ * @returns End.
+ */
+function stringEncode(str: string, dest: Uint8Array, start: number): number {
+	const l = str.length;
+	let q = !l;
+	let i = 0;
+	let next: number;
+	let chr: number;
+	let x: number;
+	if (!q) {
+		do {
+			if (!unquoted(str.charCodeAt(i++))) {
+				q = true;
+				break;
+			}
+		} while (i < l);
+		i = 0;
+	}
+	if (q) {
+		dest[start++] = 34;
+	}
+	while (i < l) {
+		if ((chr = str.charCodeAt(i++)) < 32) {
+			dest[start++] = 92;
+			if (chr < 7) {
+				next = str.charCodeAt(i);
+				if (next < 56 && next > 47) {
+					dest[start++] = 48;
+					dest[start++] = 48;
+				}
+				dest[start++] = 48 + chr;
+			} else if (chr > 13) {
+				next = str.charCodeAt(i);
+				if (next < 56 && next > 47) {
+					dest[start++] = 48;
+				}
+				dest[start++] = 48 + (chr - (x = chr % 8)) / 8;
+				dest[start++] = 48 + x;
+			} else {
+				dest[start++] = abtnvfr.charCodeAt(chr - 7);
+			}
+		} else if (chr === 34) {
+			dest[start++] = 92;
+			dest[start++] = chr;
+		} else if (chr === 92) {
+			dest[start++] = chr;
+			dest[start++] = chr;
+		} else if (chr > 126) {
+			dest[start++] = 92;
+			if (chr === 127) {
+				dest[start++] = 49;
+				dest[start++] = dest[start++] = 55;
+			} else {
+				dest[start++] = 85;
+				x = chr >> 12;
+				dest[start++] = x + (x > 9 ? 87 : 48);
+				x = chr >> 8 & 15;
+				dest[start++] = x + (x > 9 ? 87 : 48);
+				x = chr >> 4 & 15;
+				dest[start++] = x + (x > 9 ? 87 : 48);
+				x = chr & 15;
+				dest[start++] = x + (x > 9 ? 87 : 48);
+			}
+		} else {
+			dest[start++] = chr;
+		}
+	}
+	if (q) {
+		dest[start++] = 34;
+	}
+	return start;
+}
+
+/**
+ * Encode data into buffer.
+ *
+ * @param data Data.
+ * @param dest Buffer.
+ * @param start Offset.
+ * @returns End.
+ */
+function dataEncode(
+	data: Uint8Array,
+	dest: Uint8Array,
+	start: number,
+): number {
+	dest[start++] = 60;
+	for (let i = 0, c = data.length, hi, lo; i < c; i++) {
+		if (i && !(i % 4)) {
+			dest[start++] = 32;
+		}
+		hi = data[i];
+		lo = hi & 15;
+		hi >>= 4;
+		dest[start++] = hi + (hi > 9 ? 87 : 48);
+		dest[start++] = lo + (lo > 9 ? 87 : 48);
+	}
+	dest[start++] = 62;
+	return start;
+}
+
+/**
+ * Encoding options for OpenStep.
+ */
+export interface EncodeOpenStepOptions {
+	/**
+	 * Encoding format.
+	 *
+	 * @default FORMAT_OPENSTEP
+	 */
+	format?: typeof FORMAT_OPENSTEP | typeof FORMAT_STRINGS;
+
+	/**
+	 * Indentation characters.
+	 *
+	 * @default '\t'
+	 */
+	indent?: string;
+}
+
+/**
+ * Encode plist, OpenStep format.
+ *
+ * @param plist Plist object.
+ * @param options Encoding options.
+ * @returns Encoded plist.
+ */
+export function encodeOpenStep(
+	plist: PLType,
+	{ format = FORMAT_OPENSTEP, indent = '\t' }: EncodeOpenStepOptions = {},
+): Uint8Array {
+	let depth = 0;
+	let size = 1;
+	let i = 0;
+	let l = 1;
+	let e: PLType | typeof close;
+	let x: number;
+	let inDict = 0;
+	let inArray = 0;
+
+	switch (format) {
+		case FORMAT_OPENSTEP: {
+			break;
+		}
+		case FORMAT_STRINGS: {
+			if (!PLDict.is(plist)) {
+				throw new TypeError(
+					`Invalid strings root type: ${String(plist)}`,
+				);
+			}
+			depth--;
+			break;
+		}
+		default: {
+			throw new RangeError(`Unknown format: ${String(format)}`);
+		}
+	}
+
+	if (!/^[\t ]*$/.test(indent = String(indent))) {
+		throw new RangeError(`Invalid indent: ${JSON.stringify(indent)}`);
+	}
+
+	const close = Symbol();
+	const q: (PLType | typeof close)[] = [plist];
+	const ancestors = new Set<PLType>();
+	const stack: (PLArray | PLDict)[] = [];
+	const indentSize = indent.length;
+	const indentData = new Uint8Array(indentSize);
+	for (let i = indentSize; i--;) {
+		indentData[i] = indent.charCodeAt(i);
+	}
+
+	while (i < l) {
+		e = q[i++];
+
+		if (e === close) {
+			if (--depth !== -1) {
+				ancestors.delete(stack.pop()!);
+			}
+		} else if (PLString.is(e)) {
+			size += stringLength(e.value);
+		} else if (PLData.is(e)) {
+			x = e.byteLength;
+			size += x ? 2 + x + x + (x - (x % 4 || 4)) / 4 : 2;
+		} else if (PLDict.is(e)) {
+			if ((x = e.size)) {
+				size += ((depth + 1) * indentSize + 5) * x +
+					(depth !== -1 ? 3 + depth * indentSize : -1);
+				q.length = l += x += x + 1;
+				q.copyWithin(i + x, i);
+				x = i;
+				for (const [k, v] of e) {
+					q[x++] = k;
+					q[x++] = v;
+				}
+				q[x] = close;
+				if (ancestors.has(e)) {
+					throw new TypeError('Circular reference');
+				}
+				ancestors.add(e);
+				stack.push(e);
+				depth++;
+			} else if (depth !== -1) {
+				size += 2;
+			}
+		} else if (PLArray.is(e)) {
+			if ((x = e.length)) {
+				size += 2 + ((depth + 1) * indentSize + 2) * x++ +
+					depth * indentSize;
+				q.length = l += x;
+				q.copyWithin(i + x, i);
+				x = i;
+				for (const v of e) {
+					q[x++] = v;
+				}
+				q[x] = close;
+				if (ancestors.has(e)) {
+					throw new TypeError('Circular reference');
+				}
+				ancestors.add(e);
+				stack.push(e);
+				depth++;
+			} else {
+				size += 2;
+			}
+		} else {
+			throw new TypeError(`Invalid type: ${String(e)}`);
+		}
+	}
+
+	const encode = new Uint8Array(size);
+	size = i = 0;
+
+	while (i < l) {
+		e = q[i++];
+
+		if (inDict === 2) {
+			encode[size++] = 59;
+		} else if (inDict) {
+			inDict = 2;
+		}
+
+		if (e === close) {
+			if (--depth !== -1) {
+				encode[size++] = 10;
+				for (let i = depth; i--;) {
+					encode.set(indentData, size);
+					size += indentSize;
+				}
+				encode[size++] = inDict ? 125 : 41;
+			}
+			if (PLDict.is(e = stack[--stack.length - 1])) {
+				inDict = 2;
+				inArray = 0;
+			} else if (PLArray.is(e)) {
+				inArray = 2;
+				inDict = 0;
+			}
+		} else {
+			if (inDict) {
+				if (size) {
+					encode[size++] = 10;
+					for (let i = depth; i--;) {
+						encode.set(indentData, size);
+						size += indentSize;
+					}
+				}
+				size = stringEncode((e as PLString).value, encode, size);
+				encode[size++] = 32;
+				encode[size++] = 61;
+				encode[size++] = 32;
+				e = q[i++];
+			} else if (inArray) {
+				if (inArray === 2) {
+					encode[size++] = 44;
+				} else {
+					inArray = 2;
+				}
+				encode[size++] = 10;
+				for (let i = depth; i--;) {
+					encode.set(indentData, size);
+					size += indentSize;
+				}
+			}
+
+			if (PLString.is(e)) {
+				size = stringEncode(e.value, encode, size);
+			} else if (PLData.is(e)) {
+				size = dataEncode(new Uint8Array(e.buffer), encode, size);
+			} else if (PLDict.is(e)) {
+				if (e.size) {
+					if (depth !== -1) {
+						encode[size++] = 123;
+					}
+					stack.push(e);
+					depth++;
+					inDict = 1;
+					inArray = 0;
+				} else if (depth !== -1) {
+					encode[size++] = 123;
+					encode[size++] = 125;
+				}
+			} else {
+				if ((e as PLArray).length) {
+					encode[size++] = 40;
+					stack.push(e as PLArray);
+					depth++;
+					inArray = 1;
+					inDict = 0;
+				} else {
+					encode[size++] = 40;
+					encode[size++] = 41;
+				}
+			}
+		}
+	}
+
+	encode[size] = 10;
+	return encode;
+}
