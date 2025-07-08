066836ab7401b74edabdb72315094b983f2714d2
Alexander O'Mara <me@alexomara.com>
2025-07-08 01:03:07 -0400

    More examples

--- spec/fixtures/plist/real-sizes/main.c
+++ spec/fixtures/plist/real-sizes/main.c
@@ -18,18 +18,38 @@ int main() {
 		{"-0.01", -0.01},
 		{"3.14", 3.14},
 		{"-3.14", -3.14},
+		{"5e-45", 5e-45},
+		{"-5e-45", -5e-45},
+		{"5e-46", 5e-46},
+		{"-5e-46", -5e-46},
+		{"5e-324", 5e-324},
+		{"-5e-324", -5e-324},
+		{"PI", M_PI},
 		{"NAN", NAN},
 		{"INFINITY", INFINITY},
 		{"-INFINITY", -INFINITY},
 	};
 	for (int i = 0; i < sizeof(values) / sizeof(struct sample); i++) {
+		double f64 = values[i].value;
 		char name[1024] = {0};
-		sprintf(name, "f64:%s", values[i].name);
+
+		union {
+			double f;
+			uint64_t u;
+		} f64hex = {.f = f64};
+		htonll(f64hex.u);
+		sprintf(name, "f64 %016llx %s", f64hex.u, values[i].name);
 		CFStringRef key = CFStringCreateWithBytes(NULL, (const unsigned char *)name, strlen(name), kCFStringEncodingUTF8, false);
-		CFNumberRef val = CFNumberCreate(NULL, kCFNumberFloat64Type, &values[i].value);
+		CFNumberRef val = CFNumberCreate(NULL, kCFNumberFloat64Type, &f64);
 		CFDictionarySetValue(plist, key, val);
+
 		float f32 = values[i].value;
-		sprintf(name, "f32:%s", values[i].name);
+		union {
+			float f;
+			uint32_t u;
+		} f32hex = {.f = f64};
+		htonl(f32hex.u);
+		sprintf(name, "f32 %08x %s", f32hex.u, values[i].name);
 		key = CFStringCreateWithBytes(NULL, (const unsigned char *)name, strlen(name), kCFStringEncodingUTF8, false);
 		val = CFNumberCreate(NULL, kCFNumberFloat32Type, &f32);
 		CFDictionarySetValue(plist, key, val);
