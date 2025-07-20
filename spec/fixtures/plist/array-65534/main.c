e92e6067adb5de344908feecf884b2b444b27ccd
Alexander O'Mara <me@alexomara.com>
2025-07-20 01:37:21 -0400

    Added object ref size pivot specs

---
+++ spec/fixtures/plist/array-65534/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include "../../inc/shared.c"
+
+int main() {
+	CFMutableArrayRef plist = CFArrayCreateMutable(NULL, 0, NULL);
+	for (int i = 0; i < 65534; i++) {
+		CFArrayAppendValue(plist, i % 2 ? kCFBooleanTrue : kCFBooleanFalse);
+	}
+	plw(plist, CFSTR("binary.plist"), kCFPropertyListBinaryFormat_v1_0);
+	plw(plist, CFSTR("xml.plist"), kCFPropertyListXMLFormat_v1_0);
+	return 0;
+}
