0f9caffbab712596ab4821b68f8116979b43d78b
Alexander O'Mara <me@alexomara.com>
2025-08-03 02:59:55 -0400

    Updated test

--- encode/xml.test.ts
+++ encode/xml.test.ts
@@ -700,31 +700,19 @@ Deno.test('spec: integer-sizes', async () => {
 
 Deno.test('spec: integer-big', async () => {
 	const MIN = -0x80000000000000000000000000000000n;
-	const dict = new PLDict([
-		[
-			new PLString('BIG'),
-			new PLInteger(0x11121314151617180102030405060708n, 128),
-		],
-		[
-			new PLString('MAX'),
-			new PLInteger(0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFn, 128),
-		],
-		[
-			new PLString('MIN'),
-			new PLInteger(MIN, 128),
-		],
-		[
-			new PLString('MIN+1'),
-			new PLInteger(MIN + 1n, 128),
-		],
-		[
-			new PLString('MIN+2'),
-			new PLInteger(MIN + 2n, 128),
-		],
-		[
-			new PLString('SMALL'),
-			new PLInteger(42n, 128),
-		],
+	const dict = new PLArray([
+		new PLString('BIG'),
+		new PLInteger(0x11121314151617180102030405060708n, 128),
+		new PLString('SMALL'),
+		new PLInteger(42n, 128),
+		new PLString('MAX'),
+		new PLInteger(0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFn, 128),
+		new PLString('MIN'),
+		new PLInteger(MIN, 128),
+		new PLString('MIN+1'),
+		new PLInteger(MIN + 1n, 128),
+		new PLString('MIN+2'),
+		new PLInteger(MIN + 2n, 128),
 	]);
 	const encode = encodeXml(dict);
 	assertEquals(
