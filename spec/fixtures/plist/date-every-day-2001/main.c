24038c793a748715a33dbd1d1440a423cea2fc24
Alexander O'Mara <me@alexomara.com>
2025-08-03 02:30:40 -0400

    Ordered type

--- spec/fixtures/plist/date-every-day-2001/main.c
+++ spec/fixtures/plist/date-every-day-2001/main.c
@@ -3,14 +3,15 @@
 #include "../../inc/shared.c"
 
 int main() {
-	CFMutableDictionaryRef plist = CFDictionaryCreateMutable(NULL, 0, NULL, NULL);
+	CFMutableArrayRef plist = CFArrayCreateMutable(NULL, 0, NULL);
 	for (int i = 0; i < 365; i++) {
 		char keystr[1024] = {0};
 		sprintf(keystr, "%0.3d", i + 1);
 		double value = 86400 * i;
 		CFStringRef key = CFStringCreateWithBytes(NULL, (unsigned char *)keystr, strlen(keystr), kCFStringEncodingUTF8, false);
 		CFDateRef val = CFDateCreate(NULL, value);
-		CFDictionarySetValue(plist, key, val);
+		CFArrayAppendValue(plist, key);
+		CFArrayAppendValue(plist, val);
 	}
 	plw(plist, CFSTR("binary.plist"), kCFPropertyListBinaryFormat_v1_0);
 	plw(plist, CFSTR("xml.plist"), kCFPropertyListXMLFormat_v1_0);
