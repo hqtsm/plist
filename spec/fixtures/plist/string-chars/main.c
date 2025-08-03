4b00612194ce76e50a59f62fac55b8ffd15ce765
Alexander O'Mara <me@alexomara.com>
2025-08-03 03:07:35 -0400

    Ordered type

--- spec/fixtures/plist/string-chars/main.c
+++ spec/fixtures/plist/string-chars/main.c
@@ -3,14 +3,15 @@
 #include "../../inc/shared.c"
 
 int main() {
-	CFMutableDictionaryRef plist = CFDictionaryCreateMutable(NULL, 0, NULL, NULL);
-	for (int i = 0xffff; i >= 0; i--) {
+	CFMutableArrayRef plist = CFArrayCreateMutable(NULL, 0, NULL);
+	for (int i = 0; i <= 0xffff; i++) {
 		char keyi[8] = {0};
 		sprintf(keyi, "%d", i);
 		unsigned short s = i;
 		CFStringRef key = CFStringCreateWithBytes(NULL, (unsigned char *)keyi, strlen(keyi), kCFStringEncodingUTF8, false);
 		CFStringRef val = CFStringCreateWithBytes(NULL, (unsigned char *)&s, 2, kCFStringEncodingUTF16, false);
-		CFDictionarySetValue(plist, key, val);
+		CFArrayAppendValue(plist, key);
+		CFArrayAppendValue(plist, val);
 	}
 	plw(plist, CFSTR("binary.plist"), kCFPropertyListBinaryFormat_v1_0);
 	plw(plist, CFSTR("xml.plist"), kCFPropertyListXMLFormat_v1_0);
