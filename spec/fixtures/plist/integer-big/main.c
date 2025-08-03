020a51ef490a49f63739b1b6920ca2831de9b6cf
Alexander O'Mara <me@alexomara.com>
2025-08-03 02:59:48 -0400

    Ordered type

--- spec/fixtures/plist/integer-big/main.c
+++ spec/fixtures/plist/integer-big/main.c
@@ -3,32 +3,38 @@
 #include "../../inc/shared.c"
 
 int main() {
-	CFMutableDictionaryRef plist = CFDictionaryCreateMutable(NULL, 0, NULL, NULL);
+	CFMutableArrayRef plist = CFArrayCreateMutable(NULL, 0, NULL);
 	CFSInt128Struct value;
 
 	value.high = 0x1112131415161718;
 	value.low = 0x0102030405060708;
-	CFDictionarySetValue(plist, CFSTR("BIG"), CFNumberCreate(NULL, kCFNumberSInt128Type, &value));
+	CFArrayAppendValue(plist, CFSTR("BIG"));
+	CFArrayAppendValue(plist, CFNumberCreate(NULL, kCFNumberSInt128Type, &value));
 
 	value.high = 0;
 	value.low = 42;
-	CFDictionarySetValue(plist, CFSTR("SMALL"), CFNumberCreate(NULL, kCFNumberSInt128Type, &value));
+	CFArrayAppendValue(plist, CFSTR("SMALL"));
+	CFArrayAppendValue(plist, CFNumberCreate(NULL, kCFNumberSInt128Type, &value));
 
 	value.high = 0x7FFFFFFFFFFFFFFF;
 	value.low = 0xFFFFFFFFFFFFFFFF;
-	CFDictionarySetValue(plist, CFSTR("MAX"), CFNumberCreate(NULL, kCFNumberSInt128Type, &value));
+	CFArrayAppendValue(plist, CFSTR("MAX"));
+	CFArrayAppendValue(plist, CFNumberCreate(NULL, kCFNumberSInt128Type, &value));
 
 	value.high = 0x8000000000000000;
 	value.low = 0x0000000000000000;
-	CFDictionarySetValue(plist, CFSTR("MIN"), CFNumberCreate(NULL, kCFNumberSInt128Type, &value));
+	CFArrayAppendValue(plist, CFSTR("MIN"));
+	CFArrayAppendValue(plist, CFNumberCreate(NULL, kCFNumberSInt128Type, &value));
 
 	value.high = 0x8000000000000000;
 	value.low = 0x0000000000000001;
-	CFDictionarySetValue(plist, CFSTR("MIN+1"), CFNumberCreate(NULL, kCFNumberSInt128Type, &value));
+	CFArrayAppendValue(plist, CFSTR("MIN+1"));
+	CFArrayAppendValue(plist, CFNumberCreate(NULL, kCFNumberSInt128Type, &value));
 
 	value.high = 0x8000000000000000;
 	value.low = 0x0000000000000002;
-	CFDictionarySetValue(plist, CFSTR("MIN+2"), CFNumberCreate(NULL, kCFNumberSInt128Type, &value));
+	CFArrayAppendValue(plist, CFSTR("MIN+2"));
+	CFArrayAppendValue(plist, CFNumberCreate(NULL, kCFNumberSInt128Type, &value));
 
 	plw(plist, CFSTR("binary.plist"), kCFPropertyListBinaryFormat_v1_0);
 	plw(plist, CFSTR("xml.plist"), kCFPropertyListXMLFormat_v1_0);
