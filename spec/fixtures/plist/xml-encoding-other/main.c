5fde1aa8c528fb3a296d36c6c2e400c7868888b3
Alexander O'Mara <me@alexomara.com>
2025-08-12 02:05:05 -0400

    More encodings

--- spec/fixtures/plist/xml-encoding-other/main.c
+++ spec/fixtures/plist/xml-encoding-other/main.c
@@ -14,23 +14,33 @@ const char * encodings[] = {
 	"iso-8859-9",
 	"iso-8859-10",
 	"iso-8859-11",
+	"iso-8859-12",
 	"iso-8859-13",
 	"iso-8859-14",
 	"iso-8859-15",
 	"iso-8859-16",
-	"cp819",
-	"cp866",
-	"cp1250",
-	"cp1251",
-	"cp1252",
-	"cp1253",
-	"cp1254",
-	"cp1255",
-	"cp1256",
-	"cp1257",
-	"cp1258",
+	"windows-37",
+	"windows-437",
+	"windows-737",
+	"windows-775",
+	"windows-850",
+	"windows-851",
+	"windows-852",
+	"windows-855",
+	"windows-857",
+	"windows-860",
+	"windows-861",
+	"windows-862",
+	"windows-863",
+	"windows-864",
+	"windows-865",
+	"windows-866",
+	"windows-869",
 	"windows-874",
+	"windows-932",
+	"windows-936",
 	"windows-949",
+	"windows-950",
 	"windows-1250",
 	"windows-1251",
 	"windows-1252",
@@ -40,6 +50,67 @@ const char * encodings[] = {
 	"windows-1256",
 	"windows-1257",
 	"windows-1258",
+	"windows-1361",
+	"cp37",
+	"cp367",
+	"cp437",
+	"cp737",
+	"cp775",
+	"cp819",
+	"cp850",
+	"cp851",
+	"cp852",
+	"cp855",
+	"cp857",
+	"cp860",
+	"cp861",
+	"cp862",
+	"cp863",
+	"cp864",
+	"cp865",
+	"cp866",
+	"cp869",
+	"cp874",
+	"cp878",
+	"cp912",
+	"cp913",
+	"cp914",
+	"cp915",
+	"cp920",
+	"cp921",
+	"cp923",
+	"cp932",
+	"cp936",
+	"cp949",
+	"cp950",
+	"cp970",
+	"cp1089",
+	"cp1250",
+	"cp1251",
+	"cp1252",
+	"cp1253",
+	"cp1254",
+	"cp1255",
+	"cp1256",
+	"cp1257",
+	"cp1258",
+	"cp1361",
+	"cp1383",
+	"ms874",
+	"ms932",
+	"ms936",
+	"ms949",
+	"ms950",
+	"ms9500",
+	"ms9501",
+	"ms9502",
+	"ms9503",
+	"ms9504",
+	"ms9505",
+	"ms9506",
+	"ms9507",
+	"ms9508",
+	"ms9509",
 	// CF maps MacRoman CFStringEncoding (0) to error (0), maybe erroneously?
 	"macintosh"
 };
@@ -51,11 +122,34 @@ CFStringEncoding getEncoding(const char * encoding) {
 	return id;
 }
 
+char * getName(CFStringEncoding encoding) {
+	CFStringRef cfName = CFStringConvertEncodingToIANACharSetName(encoding);
+	if (!cfName) {
+		return NULL;
+	}
+	CFIndex length = CFStringGetLength(cfName);
+	CFIndex maxSize = CFStringGetMaximumSizeForEncoding(length, kCFStringEncodingUTF8) + 1;
+	char *buffer = (char *)malloc(maxSize);
+	if (!buffer) {
+		return NULL;
+	}
+	Boolean success = CFStringGetCString(cfName, buffer, maxSize, kCFStringEncodingUTF8);
+	if (!success) {
+		free(buffer);
+		return NULL;
+	}
+	return buffer;
+}
+
 int main() {
 	for (int i = 0; i < sizeof(encodings) / sizeof(char *); i++) {
 		const char * encoding = encodings[i];
 		CFStringEncoding se = getEncoding(encoding);
-		printf("%s: 0x%x\n", encoding, se);
+		char * name = getName(se);
+		printf("%s\t0x%x\t%s\n", encoding, se, name);
+		if (name) {
+			free(name);
+		}
 		char filename[1024];
 		sprintf(filename, "%s.plist", encoding);
 		FILE * fp = fopen(filename, "w");
