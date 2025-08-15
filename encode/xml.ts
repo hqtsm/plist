060e13c8f9e86b4dfda14acf85b2d312c4cd7c24
Alexander O'Mara <me@alexomara.com>
2025-08-15 00:15:21 -0400

    Private reusable UTF-8 utils

--- encode/xml.ts
+++ encode/xml.ts
@@ -5,6 +5,7 @@
  */
 
 import type { PLDate } from '../date.ts';
+import { utf8Encode, utf8Length } from '../pri/utf8.ts';
 import { FORMAT_XML_V1_0 } from '../format.ts';
 import type { PLType } from '../type.ts';
 import { walk } from '../walk.ts';
@@ -38,131 +39,6 @@ export const XML_VERSION_V0_9 = '0.9';
  * XML version 1.0.
  */
 export const XML_VERSION_V1_0 = '1.0';
-
-/**
- * Calculate string encode size.
- *
- * @param str String.
- * @param encode XML encode (0: none, 1: text, 2: attr).
- * @returns Size.
- */
-function stringLength(str: string, encode: 0 | 1 | 2 = 0): number {
-	let len = 0;
-	for (let l = str.length, i = 0, hi = 0, chr; i < l;) {
-		if ((chr = str.charCodeAt(i++)) < 128) {
-			len++;
-			if (encode) {
-				if (chr === 38) {
-					len += 4;
-				} else if (chr === 60 || chr === 62) {
-					len += 3;
-				} else if (encode === 2 && chr === 34) {
-					len += 5;
-				}
-			}
-			hi = 0;
-		} else if (chr < 2048) {
-			len += 2;
-			hi = 0;
-		} else if (chr > 55295) {
-			if (chr < 57344) {
-				if (chr < 56320) {
-					hi = 4;
-				} else {
-					len += hi;
-					hi = 0;
-				}
-			} else {
-				len += 3;
-				hi = 0;
-			}
-		} else {
-			len += 3;
-			hi = 0;
-		}
-	}
-	return len;
-}
-
-/**
- * Encode string into buffer.
- *
- * @param str String.
- * @param dest Buffer.
- * @param start Offset.
- * @param encode XML encode (0: none, 1: text, 2: attr).
- * @returns End.
- */
-function stringEncode(
-	str: string,
-	dest: Uint8Array,
-	start: number,
-	encode: 0 | 1 | 2 = 0,
-): number {
-	for (let l = str.length, i = 0, hi = 0, chr; i < l;) {
-		if ((chr = str.charCodeAt(i++)) < 128) {
-			if (encode) {
-				if (chr === 38) {
-					dest[start++] = 38;
-					dest[start++] = 97;
-					dest[start++] = 109;
-					dest[start++] = 112;
-					dest[start++] = 59;
-				} else if (chr === 60) {
-					dest[start++] = 38;
-					dest[start++] = 108;
-					dest[start++] = 116;
-					dest[start++] = 59;
-				} else if (chr === 62) {
-					dest[start++] = 38;
-					dest[start++] = 103;
-					dest[start++] = 116;
-					dest[start++] = 59;
-				} else if (encode === 2 && chr === 34) {
-					dest[start++] = 38;
-					dest[start++] = 113;
-					dest[start++] = 117;
-					dest[start++] = 111;
-					dest[start++] = 116;
-					dest[start++] = 59;
-				} else {
-					dest[start++] = chr;
-				}
-			} else {
-				dest[start++] = chr;
-			}
-			hi = 0;
-		} else if (chr < 2048) {
-			dest[start++] = 192 | (chr >> 6);
-			dest[start++] = 128 | (chr & 63);
-			hi = 0;
-		} else if (chr > 55295) {
-			if (chr < 57344) {
-				if (chr < 56320) {
-					hi = chr;
-				} else if (hi) {
-					chr += ((hi - 55296) << 10) + 9216;
-					dest[start++] = 240 | (chr >> 18);
-					dest[start++] = 128 | ((chr >> 12) & 63);
-					dest[start++] = 128 | ((chr >> 6) & 63);
-					dest[start++] = 128 | (chr & 63);
-					hi = 0;
-				}
-			} else {
-				dest[start++] = 224 | (chr >> 12);
-				dest[start++] = 128 | ((chr >> 6) & 63);
-				dest[start++] = 128 | (chr & 63);
-				hi = 0;
-			}
-		} else {
-			dest[start++] = 224 | (chr >> 12);
-			dest[start++] = 128 | ((chr >> 6) & 63);
-			dest[start++] = 128 | (chr & 63);
-			hi = 0;
-		}
-	}
-	return start;
-}
 
 /**
  * Encode integer to string.
@@ -276,9 +152,9 @@ export function encodeXml(
 	}
 
 	if (doctype) {
-		i += stringLength(doctype) + 1;
+		i += utf8Length(doctype) + 1;
 	}
-	i += stringLength(version, 2);
+	i += utf8Length(version, 2);
 
 	const a = new Set<PLType>();
 	const il = x = indent.length;
@@ -316,7 +192,7 @@ export function encodeXml(
 		},
 		key: {
 			PLDict(v): void {
-				i += 11 + stringLength(v.value, 1);
+				i += 11 + utf8Length(v.value, 1);
 			},
 		},
 		value: {
@@ -338,7 +214,7 @@ export function encodeXml(
 				i += 13 + realString(v.value).length;
 			},
 			PLString(v): void {
-				i += 17 + stringLength(v.value, 1);
+				i += 17 + utf8Length(v.value, 1);
 			},
 			PLUID(v, d): void {
 				i += 52 + v.value.toString().length + d++ * il + d * il * 2;
@@ -355,13 +231,13 @@ export function encodeXml(
 	});
 
 	const r = new Uint8Array(i);
-	i = stringEncode(xmlHeader, r, 0);
+	i = utf8Encode(xmlHeader, r, 0);
 	r[i++] = 10;
-	i = stringEncode(doctype, r, i);
+	i = utf8Encode(doctype, r, i);
 	r[i++] = 10;
-	i = stringEncode('<plist version="', r, i);
-	i = stringEncode(version, r, i, 2);
-	i = stringEncode('">', r, i);
+	i = utf8Encode('<plist version="', r, i);
+	i = utf8Encode(version, r, i, 2);
+	i = utf8Encode('">', r, i);
 	r[i++] = 10;
 
 	walk(plist, {
@@ -371,11 +247,11 @@ export function encodeXml(
 					r.set(id, i);
 				}
 				if (v.length) {
-					i = stringEncode('<array>', r, i);
+					i = utf8Encode('<array>', r, i);
 					r[i++] = 10;
 					return;
 				}
-				i = stringEncode('<array/>', r, i);
+				i = utf8Encode('<array/>', r, i);
 				r[i++] = 10;
 				return 1;
 			},
@@ -384,11 +260,11 @@ export function encodeXml(
 					r.set(id, i);
 				}
 				if (v.size) {
-					i = stringEncode('<dict>', r, i);
+					i = utf8Encode('<dict>', r, i);
 					r[i++] = 10;
 					return;
 				}
-				i = stringEncode('<dict/>', r, i);
+				i = utf8Encode('<dict/>', r, i);
 				r[i++] = 10;
 				return 1;
 			},
@@ -398,9 +274,9 @@ export function encodeXml(
 				for (; d--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode('<key>', r, i);
-				i = stringEncode(v.value, r, i, 1);
-				i = stringEncode('</key>', r, i);
+				i = utf8Encode('<key>', r, i);
+				i = utf8Encode(v.value, r, i, 1);
+				i = utf8Encode('</key>', r, i);
 				r[i++] = 10;
 			},
 		},
@@ -409,14 +285,14 @@ export function encodeXml(
 				for (; d--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode(v.value ? '<true/>' : '<false/>', r, i);
+				i = utf8Encode(v.value ? '<true/>' : '<false/>', r, i);
 				r[i++] = 10;
 			},
 			PLData(v, d): void {
 				for (x = d; x--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode('<data>', r, i);
+				i = utf8Encode('<data>', r, i);
 				r[i++] = 10;
 				for (
 					let u = new Uint8Array(v.buffer),
@@ -455,67 +331,67 @@ export function encodeXml(
 				for (; d--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode('</data>', r, i);
+				i = utf8Encode('</data>', r, i);
 				r[i++] = 10;
 			},
 			PLDate(v, d): void {
 				for (; d--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode('<date>', r, i);
-				i = stringEncode(dateString(v), r, i);
-				i = stringEncode('</date>', r, i);
+				i = utf8Encode('<date>', r, i);
+				i = utf8Encode(dateString(v), r, i);
+				i = utf8Encode('</date>', r, i);
 				r[i++] = 10;
 			},
 			PLInteger(v, d): void {
 				for (; d--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode('<integer>', r, i);
-				i = stringEncode(integerString(v.value), r, i);
-				i = stringEncode('</integer>', r, i);
+				i = utf8Encode('<integer>', r, i);
+				i = utf8Encode(integerString(v.value), r, i);
+				i = utf8Encode('</integer>', r, i);
 				r[i++] = 10;
 			},
 			PLReal(v, d): void {
 				for (; d--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode('<real>', r, i);
-				i = stringEncode(realString(v.value), r, i);
-				i = stringEncode('</real>', r, i);
+				i = utf8Encode('<real>', r, i);
+				i = utf8Encode(realString(v.value), r, i);
+				i = utf8Encode('</real>', r, i);
 				r[i++] = 10;
 			},
 			PLString(v, d): void {
 				for (; d--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode('<string>', r, i);
-				i = stringEncode(v.value, r, i, 1);
-				i = stringEncode('</string>', r, i);
+				i = utf8Encode('<string>', r, i);
+				i = utf8Encode(v.value, r, i, 1);
+				i = utf8Encode('</string>', r, i);
 				r[i++] = 10;
 			},
 			PLUID(v, d): void {
 				for (x = d++; x--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode('<dict>', r, i);
+				i = utf8Encode('<dict>', r, i);
 				r[i++] = 10;
 				for (x = d; x--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode('<key>CF$UID</key>', r, i);
+				i = utf8Encode('<key>CF$UID</key>', r, i);
 				r[i++] = 10;
 				for (x = d--; x--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode('<integer>', r, i);
-				i = stringEncode(v.value.toString(), r, i);
-				i = stringEncode('</integer>', r, i);
+				i = utf8Encode('<integer>', r, i);
+				i = utf8Encode(v.value.toString(), r, i);
+				i = utf8Encode('</integer>', r, i);
 				r[i++] = 10;
 				for (; d--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode('</dict>', r, i);
+				i = utf8Encode('</dict>', r, i);
 				r[i++] = 10;
 			},
 		},
@@ -524,20 +400,20 @@ export function encodeXml(
 				for (; d--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode('</array>', r, i);
+				i = utf8Encode('</array>', r, i);
 				r[i++] = 10;
 			},
 			PLDict(_, d): void {
 				for (; d--; i += il) {
 					r.set(id, i);
 				}
-				i = stringEncode('</dict>', r, i);
+				i = utf8Encode('</dict>', r, i);
 				r[i++] = 10;
 			},
 		},
 	});
 
-	i = stringEncode('</plist>', r, i);
+	i = utf8Encode('</plist>', r, i);
 	r[i] = 10;
 	return r;
 }
---
+++ pri/utf8.ts
@@ -0,0 +1,130 @@
+/**
+ * @module
+ *
+ * UTF-8 utils.
+ */
+
+/**
+ * Calculate string encode size.
+ *
+ * @param str String.
+ * @param encode XML encode (0: none, 1: text, 2: attr).
+ * @returns Size.
+ */
+export function utf8Length(str: string, encode: 0 | 1 | 2 = 0): number {
+	let len = 0;
+	for (let l = str.length, i = 0, hi = 0, chr; i < l;) {
+		if ((chr = str.charCodeAt(i++)) < 128) {
+			len++;
+			if (encode) {
+				if (chr === 38) {
+					len += 4;
+				} else if (chr === 60 || chr === 62) {
+					len += 3;
+				} else if (encode === 2 && chr === 34) {
+					len += 5;
+				}
+			}
+			hi = 0;
+		} else if (chr < 2048) {
+			len += 2;
+			hi = 0;
+		} else if (chr > 55295) {
+			if (chr < 57344) {
+				if (chr < 56320) {
+					hi = 4;
+				} else {
+					len += hi;
+					hi = 0;
+				}
+			} else {
+				len += 3;
+				hi = 0;
+			}
+		} else {
+			len += 3;
+			hi = 0;
+		}
+	}
+	return len;
+}
+
+/**
+ * Encode string into buffer.
+ *
+ * @param str String.
+ * @param dest Buffer.
+ * @param start Offset.
+ * @param encode XML encode (0: none, 1: text, 2: attr).
+ * @returns End.
+ */
+export function utf8Encode(
+	str: string,
+	dest: Uint8Array,
+	start: number,
+	encode: 0 | 1 | 2 = 0,
+): number {
+	for (let l = str.length, i = 0, hi = 0, chr; i < l;) {
+		if ((chr = str.charCodeAt(i++)) < 128) {
+			if (encode) {
+				if (chr === 38) {
+					dest[start++] = 38;
+					dest[start++] = 97;
+					dest[start++] = 109;
+					dest[start++] = 112;
+					dest[start++] = 59;
+				} else if (chr === 60) {
+					dest[start++] = 38;
+					dest[start++] = 108;
+					dest[start++] = 116;
+					dest[start++] = 59;
+				} else if (chr === 62) {
+					dest[start++] = 38;
+					dest[start++] = 103;
+					dest[start++] = 116;
+					dest[start++] = 59;
+				} else if (encode === 2 && chr === 34) {
+					dest[start++] = 38;
+					dest[start++] = 113;
+					dest[start++] = 117;
+					dest[start++] = 111;
+					dest[start++] = 116;
+					dest[start++] = 59;
+				} else {
+					dest[start++] = chr;
+				}
+			} else {
+				dest[start++] = chr;
+			}
+			hi = 0;
+		} else if (chr < 2048) {
+			dest[start++] = 192 | (chr >> 6);
+			dest[start++] = 128 | (chr & 63);
+			hi = 0;
+		} else if (chr > 55295) {
+			if (chr < 57344) {
+				if (chr < 56320) {
+					hi = chr;
+				} else if (hi) {
+					chr += ((hi - 55296) << 10) + 9216;
+					dest[start++] = 240 | (chr >> 18);
+					dest[start++] = 128 | ((chr >> 12) & 63);
+					dest[start++] = 128 | ((chr >> 6) & 63);
+					dest[start++] = 128 | (chr & 63);
+					hi = 0;
+				}
+			} else {
+				dest[start++] = 224 | (chr >> 12);
+				dest[start++] = 128 | ((chr >> 6) & 63);
+				dest[start++] = 128 | (chr & 63);
+				hi = 0;
+			}
+		} else {
+			dest[start++] = 224 | (chr >> 12);
+			dest[start++] = 128 | ((chr >> 6) & 63);
+			dest[start++] = 128 | (chr & 63);
+			hi = 0;
+		}
+	}
+	return start;
+}
