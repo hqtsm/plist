ee0bc537a528f688acc797bb3f8fe7f74ef657c7
Alexander O'Mara <me@alexomara.com>
2025-08-23 01:43:58 -0400

    Added x-nextstep mapping notes

---
+++ spec/fixtures/encoding/x-nextstep/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <CoreFoundation/CoreFoundation.h>
+
+int main() {
+	FILE * fp = fopen("x-nextstep.txt", "w");
+	for (int a = 0; a < 256; a++) {
+		unsigned char byte = a;
+		CFStringRef str = CFStringCreateWithBytes(NULL, &byte, 1, kCFStringEncodingNextStepLatin, false);
+		fprintf(fp, "%0.02X =", a);
+		if (str) {
+			for (CFIndex i = 0, l = CFStringGetLength(str); i < l; i++) {
+				fprintf(fp, " %d", CFStringGetCharacterAtIndex(str, i));
+			}
+		}
+		fprintf(fp, "\n");
+	}
+	fclose(fp);
+	return 0;
+}
