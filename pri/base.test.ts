6451823cb61a3e6dd881843a3329ba9bc513c17a
Alexander O'Mara <me@alexomara.com>
2025-09-07 18:53:26 -0400

    Simpler test

--- pri/base.test.ts
+++ pri/base.test.ts
@@ -1,82 +1,15 @@
 import { assertEquals, assertLess } from '@std/assert';
 import { b16d, b64d, b64e } from './base.ts';
 
+const hex = '0123456789abcdef';
 const b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 
 Deno.test('b16d', () => {
 	for (let c = 0; c < 256; c++) {
 		const s = String.fromCharCode(c);
 		const tag = `${c}: ${JSON.stringify(s)}`;
-		switch (s.toLowerCase()) {
-			case '0': {
-				assertEquals(b16d(c), 0, tag);
-				break;
-			}
-			case '1': {
-				assertEquals(b16d(c), 1, tag);
-				break;
-			}
-			case '2': {
-				assertEquals(b16d(c), 2, tag);
-				break;
-			}
-			case '3': {
-				assertEquals(b16d(c), 3, tag);
-				break;
-			}
-			case '4': {
-				assertEquals(b16d(c), 4, tag);
-				break;
-			}
-			case '5': {
-				assertEquals(b16d(c), 5, tag);
-				break;
-			}
-			case '6': {
-				assertEquals(b16d(c), 6, tag);
-				break;
-			}
-			case '7': {
-				assertEquals(b16d(c), 7, tag);
-				break;
-			}
-			case '8': {
-				assertEquals(b16d(c), 8, tag);
-				break;
-			}
-			case '9': {
-				assertEquals(b16d(c), 9, tag);
-				break;
-			}
-			case 'a': {
-				assertEquals(b16d(c), 10, tag);
-				break;
-			}
-			case 'b': {
-				assertEquals(b16d(c), 11, tag);
-				break;
-			}
-			case 'c': {
-				assertEquals(b16d(c), 12, tag);
-				break;
-			}
-			case 'd': {
-				assertEquals(b16d(c), 13, tag);
-				break;
-			}
-			case 'e': {
-				assertEquals(b16d(c), 14, tag);
-				break;
-			}
-			case 'f': {
-				assertEquals(b16d(c), 15, tag);
-				break;
-			}
-			default: {
-				assertEquals(b16d(c), -1, tag);
-				break;
-			}
-		}
+		const e = hex.indexOf(s.toLowerCase());
+		assertEquals(b16d(c), e, tag);
 	}
 });
 
