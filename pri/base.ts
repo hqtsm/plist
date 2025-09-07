6719c738773fd625750b35f6fa85f5587c16f88d
Alexander O'Mara <me@alexomara.com>
2025-09-07 14:00:02 -0400

    Small optimization

--- pri/base.ts
+++ pri/base.ts
@@ -5,10 +5,4 @@
  * @returns Value, or -1 for invalid.
  */
 export const b16d = (c: number): number =>
-	c < 58
-		? c < 48 ? -1 : c - 48
-		: c > 96
-		? c < 103 ? c - 87 : -1
-		: c < 71 && c > 64
-		? c - 55
-		: -1;
+	c < 58 ? c < 48 ? -1 : c - 48 : (c |= 32) > 96 && c < 103 ? c - 87 : -1;
